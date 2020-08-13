7cb709a35a2690f5b71631f402b54a23653888d2
gmurdoko <galangcahyomurdoko@gmail.com>
2020-08-13 14:16:01 +0700

    Navigation

---
+++ app/src/main/java/com/example/exercise_navigation/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.exercise_navigation
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import androidx.navigation.NavController
+import kotlinx.android.synthetic.main.fragment_home.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+
+
+
+}
